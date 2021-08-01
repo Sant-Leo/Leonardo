f1914da5b117ffa9350f9b7613fbb575e8f0afe6
Leonardo dos Santos <lps11154@gmail.com>
2021-08-01 10:30:05 -0300

    Versão 1.1

--- prova.cpp
+++ prova.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 int main() { 
  string nome;
+ int I;
+ string O;
  cout << "Digite seu nome." << endl  ;
  cin >> nome ;
  cout << "Ola"<< "," << nome << "!" << endl ;
- }
+ cout << " Digite sua idade." << endl ;
+ cin >> I ;
+ cout << nome <<", " << I << " anos" << endl;
+}
